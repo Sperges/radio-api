847afad2225585ec7016384e59c1a430c47ed163
rnbiv <rnbiv45@gmail.com>
2022-06-02 14:58:59 -0400

    remove uuid

--- models/models.go
+++ models/models.go
@@ -1,20 +1,18 @@
 package models
 
-import "github.com/google/uuid"
-
 type User struct {
 	ID   int    `json:"Id"`
 	Name string `json:"Name"`
 }
 
 type Radio struct {
-	ID     uuid.UUID `json:"Id"`
-	Name   string    `json:"Name"`
-	UserId int       `json:"UserId"`
+	ID     int    `json:"Id"`
+	Name   string `json:"Name"`
+	UserId int    `json:"UserId"`
 }
 
 type Playlist struct {
-	ID     uuid.UUID `json:"Id"`
-	Name   string    `json:"Name"`
-	UserId int       `json:"UserId"`
+	ID     int    `json:"Id"`
+	Name   string `json:"Name"`
+	UserId int    `json:"UserId"`
 }
