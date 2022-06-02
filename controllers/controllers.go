bf357aeadee325ffad75dfaa4541145868fb54ef
rnbiv <rnbiv45@gmail.com>
2022-06-02 14:59:53 -0400

    fix bad paths

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -11,14 +11,12 @@ func SetupRoutes(r *gin.Engine) {
 	r.POST("/radio", CreateRadio)
 	r.GET("/radio/:id", ReadRadio)
 	r.GET("/:user/radios/:id", ReadRadioByIDFromUser)
-	r.GET("/:user/radios/:name", ReadRadioByNameFromUser)
 	r.PUT("/radio/:id", UpdateRadio)
 	r.DELETE("/radio/:id", DeleteRadio)
 
 	r.POST("/playlist", CreatePlaylist)
 	r.GET("/playlist/:id", ReadPlaylist)
 	r.GET("/:user/playlists/:id", ReadPlaylistByIDFromUser)
-	r.GET("/:user/playlists/:name", ReadPlaylistByNameFromUser)
 	r.PUT("/playlist/:id", UpdatePlaylist)
 	r.DELETE("/playlist/:id", DeletePlaylist)
 
